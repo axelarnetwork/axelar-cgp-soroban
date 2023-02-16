fac6ded9242f9a6ec7731719f571516835c757b7
fahimahmedx <fahim@axelar.network>
2023-02-16 16:54:30 -0500

    start implementing execute() function

--- axelar-network/src/lib.rs
+++ axelar-network/src/lib.rs
@@ -1,17 +1,42 @@
 #![no_std]
+use soroban_sdk::{contractimpl, Bytes, BytesN, Env, Symbol, Vec, Address, map};
+use ethabi::{encode, decode};
+use sha3::Keccak256;
 
-use soroban_sdk::{contractimpl, Bytes, BytesN, Env, Symbol, Vec, Address};
+const SELECTOR_TRANSFER_OPERATORSHIP: u8 = 0;
+const SELECTOR_APPROVE_CONTRACT_CALL: u8 = 1;
 
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
+    pub fn execute(
+        chainId: u128,
+        commandIds: Vec<BytesN<32>>,
+        commands: Vec<u8>,
+        params: Vec<Bytes>
+    ) {
+        let commandsLength: i128 = commandIds.len();
+
+        if (commandsLength != commands.len() || commandsLength != params.len()) {
+            // implement
+        }
+
+        for i in 0..commandsLength {
+            let commandId: BytesN<32> = commandIds[i];
+            // implement
+        }
+
+
+
+    }
+
     pub fn approve( // approveContractCall
         env: Env,
         commandId: BytesN<32>,
         sourceChain: Symbol,
         sourceAddress: Address,
-        destinationAddress: Address,
+        contractAddress: Address,
         payloadHash: BytesN<32>,
         sourceTxHash: BytesN<32>,
         sourceEventIndex: u128
