26728fd4355b09759d010948e4f2fc35af40ae41
fahimahmedx <fahim@axelar.network>
2023-02-17 16:58:51 -0500

    work on changing input of execute.

--- axelar-network/src/lib.rs
+++ axelar-network/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Bytes, BytesN, Env, Symbol, Vec, Address, map};
-use ethabi::{encode, decode};
+use soroban_sdk::{contractimpl, Bytes, BytesN, Env, Symbol, Vec, vec, Address, map};
+use ethabi::{encode, decode, ParamType};
+use crate::utils::{self, abi_encode, abi_decode, clean_payload};
 use sha3::Keccak256;
 
 const SELECTOR_TRANSFER_OPERATORSHIP: u8 = 0;
@@ -11,19 +12,29 @@ pub struct Contract;
 #[contractimpl]
 impl Contract {
     pub fn execute(
-        chainId: u128,
-        commandIds: Vec<BytesN<32>>,
-        commands: Vec<u8>,
-        params: Vec<Bytes>
+        env: Env,
+        input: Bytes
+        // chainId: u128,
+        // commandIds: Vec<BytesN<32>>,
+        // commands: Vec<u32>,
+        // params: Vec<Bytes>
     ) {
-        let commandsLength: i128 = commandIds.len();
+        //let payload = clean_payload(input.clone());
+        // Assume that input is a cleaned payload. That is, a payload without the 0x at the start.
+        let tokens = abi_decode(&payload, &[ParamType::Bytes]);//.unwrap();
+        // current issue: the type of payload doesn't match up with the parameter type for abi_decode.
+
+        let data = tokens[0].clone().into_bytes().unwrap();
+        let proof = tokens[1].clone().into_bytes().unwrap();
+
+        let commandsLength: u32 = commandIds.len();
 
         if (commandsLength != commands.len() || commandsLength != params.len()) {
             // implement
         }
 
         for i in 0..commandsLength {
-            let commandId: BytesN<32> = commandIds[i];
+            let commandId: BytesN<32> = commandIds.get(i);
             // implement
         }
 
