2e9801d542b496e4f7f298cd2e7c74f2d673ceae
fahimahmedx <fahim@axelar.network>
2023-03-08 12:25:46 -0500

    implement approveContractCall logic in execute

--- axelar-network/src/lib.rs
+++ axelar-network/src/lib.rs
@@ -12,7 +12,7 @@ extern crate alloc;
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Data {
     pub chain_id: u64,
-    pub commandids: Vec<Bytes>,
+    pub commandids: Vec<BytesN<32>>,
     pub commands: Vec<Bytes>, //instead of Vec<String>
     pub params: Vec<Bytes>
 }
@@ -66,6 +66,13 @@ pub struct ContractCall {
     pub payload: Bytes
 }
 
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ExecutedEvent {
+    pub command_id: BytesN<32>
+}
+
+
 pub struct Contract;
 mod utils;
 mod test;
@@ -78,7 +85,7 @@ impl Contract {
     ) {
         
         // dummy values below
-        let SELECTOR_TRANSFER_OPERATORSHIP: BytesN<32> = bytesn!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d);
+        let SELECTOR_TRANSFER_OPERATORSHIP: BytesN<32> = bytesn!(&env, 0xabcd3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d);
         let SELECTOR_APPROVE_CONTRACT_CALL: BytesN<32> = bytesn!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d);
         
         let data: Data = input.data;
@@ -87,7 +94,7 @@ impl Contract {
         let mut allowOperatorshipTransfer: bool = false; // implement
 
         let chain_id: u64 = data.chain_id;
-        let command_ids: Vec<Bytes> = data.commandids;
+        let command_ids: Vec<BytesN<32>> = data.commandids;
         let commands: Vec<Bytes> = data.commands;
         let params: Vec<Bytes> = data.params;
 
@@ -98,10 +105,9 @@ impl Contract {
         //     // implement
         // }
         for i in 0..commands_length {
-            let command_id: Bytes = command_ids.get(i).unwrap().unwrap();
-
-            let command_selector: BytesN<4>;
-            let command_hash: BytesN<32> = env.crypto().sha256(&command_id);
+            let command_id: BytesN<32> = command_ids.get(i).unwrap().unwrap();
+            let command_hash: BytesN<32> = env.crypto().sha256(&commands.get(i).unwrap().unwrap());
+            let mut success: bool = false;
 
             if command_hash == SELECTOR_TRANSFER_OPERATORSHIP {
                 if (!allowOperatorshipTransfer) {
@@ -109,13 +115,20 @@ impl Contract {
                 }
                 allowOperatorshipTransfer = false;
                 // implement
+
             }
             else if command_hash == SELECTOR_APPROVE_CONTRACT_CALL {
-                //approve(env, );
-                // implement
+                Self::_setCommandExecuted(env.clone(), command_id.clone(), true);
+                success = Self::approve(env.clone(), params.get(i).unwrap().unwrap(), command_id.clone());
             }
 
-            // implement
+            if (success) {
+                let event: ExecutedEvent = ExecutedEvent { command_id: command_id.clone() };
+                env.events().publish((), event);
+
+            } else {
+                Self::_setCommandExecuted(env.clone(), command_id.clone(), false);
+            }
         }
 
 
@@ -124,9 +137,9 @@ impl Contract {
 
     pub fn approve( // approveContractCall
         env: Env,
-        params: Bytes,//ContractPayload,
+        params: Bytes,
         command_id: BytesN<32>
-    ) {
+    ) -> bool {
         let decoded: ContractPayload = ContractPayload::deserialize(&env, &params).unwrap();
         let src_chain: Bytes = decoded.src_chain;
         let src_addr: Bytes = decoded.src_add;
@@ -136,12 +149,10 @@ impl Contract {
         let src_event: u64 = decoded.src_evnt;
         
         Self::_setContractCallApproved(env.clone(), command_id.clone(), src_chain.clone(), src_addr.clone(), contract.clone(), payload_ha.clone());
-
         let event: ContractCallApprovedEvent = ContractCallApprovedEvent { src_chain, src_addr, src_tx, src_event};
-        // hash the payload, use storage.set() with hash as key, and set as true?
-
-        // let data = map![&env, (1, sourceChain), (2, sourceAddress), (3, contractAddress), (4, sourceTxHash), (5, sourceEventIndex)];
         env.events().publish((command_id, contract, payload_ha), event);
+
+        true
     }
     
     fn _setContractCallApproved( // how do I make this functio internal / protected
@@ -162,6 +173,15 @@ impl Contract {
         };
         let key: BytesN<32> = env.crypto().sha256(&data.serialize(&env));
         env.storage().set(&key, &true);
+    }
+
+    fn _setCommandExecuted(
+        env: Env,
+        command_id: BytesN<32>,
+        executed: bool
+    ) {
+        let key: BytesN<32> = env.crypto().sha256(&command_id.serialize(&env));
+        env.storage().set(&key, &executed);
     }
 
     pub fn transferOp( // transferOperatorship
