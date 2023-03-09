6382284ced7dba4fddb873ab5997414c493da8d4
fahimahmedx <fahim@axelar.network>
2023-03-09 11:45:49 -0500

    The mock test passes!

--- axelar-network/src/test.rs
+++ axelar-network/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{bytes, vec, Env};
+use soroban_sdk::{testutils::Events, bytes, vec, Env, IntoVal};
 extern crate std;
 
 #[test]
@@ -23,11 +23,38 @@ fn test() {
         chain_id: 1,
         commandids: vec![&env, bytesn!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d)],
         commands: vec![&env, bytes![&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d]],
-        params: vec![&env, params.serialize(&env)]
+        params: vec![&env, params.clone().serialize(&env)]
     };
 
     let input: Input = Input {
-        data: data,
+        data: data.clone(),
         proof: bytes![&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d]
     };
+
+    let test = input.serialize(&env);
+    client.execute(&test);
+    let event: ContractCallApprovedEvent = ContractCallApprovedEvent { src_chain: params.src_chain, src_addr: params.src_add, src_tx: params.src_tx_ha, src_event: params.src_evnt};
+    let event2: ExecutedEvent = ExecutedEvent { command_id: data.commandids.get(0).unwrap().unwrap() };
+    assert_eq!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (
+                bytes!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d),
+                bytes!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d),
+                bytes!(&env, 0xfded3f55dec47250a52a8c0bb7038e72fa6ffaae33562f77cd2b629ef7fd424d),
+                ).into_val(&env),
+                event.into_val(&env)
+            ),
+            (
+                contract_id.clone(),
+                ().into_val(&env),
+                event2.into_val(&env)
+            )
+        ]
+    );
+
+
 }
