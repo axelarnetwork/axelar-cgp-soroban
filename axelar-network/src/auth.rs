58cd84623e176265d88e3e6042caf8e1cbe6ff6e
fahimahmedx <fahim@axelar.network>
2023-03-14 13:02:12 -0400

    implement validate_proof

--- axelar-network/src/auth.rs
+++ axelar-network/src/auth.rs
@@ -1,6 +1,8 @@
 use soroban_sdk::{contractimpl, contracttype, bytes, Bytes, BytesN, Env, Address, Map, Vec, crypto,
-    serde::{Deserialize, Serialize}, xdr::Uint256
+    serde::{Deserialize, Serialize}, xdr::Uint256, symbol
 };
+
+use crate::Operatorship;
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -9,6 +11,15 @@ pub struct Axelar {
     crnt_epoch: u64, //current_epoch
     hash_epoch: Map<u64, BytesN<32>>, // hash_for_epoch
     epoch_hash: Map<BytesN<32>, u64>, // epoch_for_hash
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Validate {
+    pub operators: Vec<Address>,
+    pub weights: Vec<u128>, // uint256
+    pub threshold: u128, // uint256
+    pub signatures: Vec<Bytes>
 }
 
     pub fn transferOp( // transferOperatorship
@@ -49,7 +60,7 @@ pub struct Axelar {
             //implementation: make variables all in one big hash, but the hash for epoch map is prefixed.
             
 
-        }//
+        //}
 
     }
 
@@ -61,13 +72,45 @@ fn toSignedMsgHsh(
 // can then use this for the validateProof & it wont have an impact as it's also made up on axelar side
 }
 
-fn validateProof() {
+fn validate_proof(
+    env: Env,
+    msghash: BytesN<32>,
+    proof: Bytes
+) -> bool {
+    let tokens: Validate = Validate::deserialize(&env, &proof).unwrap();
+    let operators: Vec<Address> = tokens.operators;
+    let weights: Vec<u128> = tokens.weights;
+    let threshold: u128 = tokens.threshold;
+    let signatures: Vec<Bytes> = tokens.signatures;
+
+    let operator: Operatorship = Operatorship {
+        new_ops: operators.clone(),
+        new_wghts: weights.clone(),
+        new_thres: threshold
+    };
+    let operators_hash: BytesN<32> = env.crypto().sha256(&operator.serialize(&env));
+
+    let operators_epoch: u128 = env.storage().get(operators_hash).unwrap().unwrap(); //uint256
+    let epoch: u128 = env.storage().get(symbol!("cur_epoch")).unwrap().unwrap(); //uint256
+
+    if (operators_epoch == 0 || epoch - operators_epoch >= 16) {
+        // implement
+    }
+
+    validateSig(env, msghash, operators, weights, threshold, signatures);
+
+    return operators_epoch == epoch;
+    
 
 }
 
 fn validateSig(
-    hash: BytesN<32>,
-    proof: Bytes
+    env: Env,
+    msghash: BytesN<32>,
+    operators: Vec<Address>,
+    weights: Vec<u128>, //uint256
+    threshold: u128, //uint256
+    signatures: Vec<Bytes> 
 ) {
     // what is ECDSA.recover()?
 
