fc8b43a7c6784c4245174577a50b536025c7d291
fahimahmedx <fahim@axelar.network>
2023-04-21 16:34:18 -0400

    implement functions for Axelar Executable

--- executable/src/executable.rs
+++ executable/src/executable.rs
@@ -4,29 +4,64 @@ use soroban_sdk::{contractimpl, contracttype, contracterror, bytes, Bytes, Bytes
 
 mod gateway {
     soroban_sdk::contractimport!(
-        file = "../../target/wasm32-unknown-unknown/release/contract.wasm"
+        file = "../contract/target/wasm32-unknown-unknown/release/contract.wasm"
     );
 }
 
-pub struct Executable;
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    NotApprovedByGateway = 8,
+}
 
-#[contractimpl]
-impl Executable {
+/// This is a trait that is implemented by the contract and provides a contract-specific way to execute a command.
+pub trait ContractExecutable {
+    fn _execute(env: Env, source_chain: String, source_address: String, payload: Bytes);
+}
 
-    pub fn execute(
+pub trait Executable {
+    fn execute(
         env: Env,
         contract_id: BytesN<32>,
         command_id: BytesN<32>,
         source_chain: String,
         source_address: String,
+        contract_address: String, // because soroban does not have msg.sender
         payload: Bytes
-    ) {
-        let client = gateway::Client::new(&env, &contract_id);
-        let payload_hash: BytesN<32> = env.crypto().sha256(&payload);
+    );
 
+}
 
+/// A macro that is used to implement the AxelarExecutable trait for the contract.
+#[macro_export]
+macro_rules! impl_axelar_executable {
+    ($contract: ident, $gateway_account_id: ident, $_execute: ident) => {
+
+        #[contractimpl]
+        impl Executable for $contract {
         
+            pub fn execute(
+                env: Env,
+                gateway_contract_id: BytesN<32>,
+                command_id: BytesN<32>,
+                source_chain: String,
+                source_address: String,
+                contract_address: String, // because soroban does not have msg.sender
+                payload: Bytes
+            ) {
+                let client = gateway::Client::new(&env, &gateway_contract_id);
+                let payload_hash: BytesN<32> = env.crypto().sha256(&payload);
+        
+                if (!client.validate_contract_call(&command_id, &source_chain, &source_address, &contract_address, &payload_hash)) {
+                    panic_with_error!(env, Error::NotApprovedByGateway);
+                }
+
+                // call _execute()
+
+            }
+        
+        }
 
     }
-
 }
