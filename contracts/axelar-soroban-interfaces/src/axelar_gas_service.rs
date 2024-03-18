884d414e762c03507388b680754b05b725a9f534
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-03-18 18:16:00 -0400

    Update contracts/axelar-soroban-interfaces/src/axelar_gas_service.rs
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- contracts/axelar-soroban-interfaces/src/axelar_gas_service.rs
+++ contracts/axelar-soroban-interfaces/src/axelar_gas_service.rs
@@ -4,7 +4,7 @@ use soroban_sdk::{Address, Bytes, Env, String, U256};
 /// Interface for the Axelar Gas Service.
 // #[contractclient(crate_path = "crate", name = "AxelarGasService")]
 pub trait AxelarGasServiceInterface {
-    /// Pay for gas using native currency for a contract call on a destination chain. This function is called on the source chain before calling the gateway to execute a remote contract.
+    /// Pay for gas using a token for a contract call on a destination chain. This function is called on the source chain before calling the gateway to execute a remote contract.
     fn pay_gas_for_contract_call(
         env: Env,
         sender: Address,
