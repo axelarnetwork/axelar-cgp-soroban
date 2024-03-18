4be6a5173d0cbaea8d8501b3b29d282b519c1b32
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-03-18 18:18:24 -0400

    Update packages/axelar-soroban-std/src/types.rs
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- packages/axelar-soroban-std/src/types.rs
+++ packages/axelar-soroban-std/src/types.rs
@@ -4,7 +4,7 @@ pub type Hash = BytesN<32>;
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct TokenDetails {
-    pub token_addr: Address,
+pub struct Token {
+    pub address: Address, // TODO: check if this can be changed to a TokenClient type instead which is richer than Address, or a generic type implementing TokenInterface
     pub amount: i128,
 }
