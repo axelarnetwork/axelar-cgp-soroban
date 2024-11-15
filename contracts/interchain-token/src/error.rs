a325b6e016da8d985ca95ac771a449dc935eb42c
Laith Bahodi <laith@interoplabs.io>
2024-11-15 13:00:50 -0500

    remove interchain-token error type
    only has two variants which aren't used. leave commented out to indicate
    it should be used if an error is ever needed in the future (?)

--- contracts/interchain-token/src/error.rs
+++ contracts/interchain-token/src/error.rs
@@ -1,9 +1,6 @@
-use soroban_sdk::contracterror;
+// use soroban_sdk::contracterror;
 
-#[contracterror]
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-#[repr(u32)]
-pub enum ContractError {
-    NotInitialized = 1,
-    AlreadyInitialized = 2,
-}
+// #[contracterror]
+// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+// #[repr(u32)]
+// pub enum ContractError {}
