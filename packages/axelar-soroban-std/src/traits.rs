4bb1caf729db863d1672bbc51440bea00a0a3845
Christian Gorenflo <christian@axelar.network>
2024-11-18 10:54:18 -0500

    introduce testutils submodule

--- packages/axelar-soroban-std/src/traits.rs
+++ packages/axelar-soroban-std/src/traits.rs
@@ -1,18 +1,5 @@
-extern crate std;
-
-use soroban_sdk::{Env, IntoVal, TryFromVal, Val, Vec};
-
 #[cfg(any(test, feature = "testutils"))]
-pub trait IntoVec<T> {
-    fn into_vec(self, env: &Env) -> Vec<T>;
-}
-
-#[cfg(any(test, feature = "testutils"))]
-impl<T: Clone + IntoVal<Env, Val> + TryFromVal<Env, Val>> IntoVec<T> for std::vec::Vec<T> {
-    fn into_vec(self, env: &Env) -> Vec<T> {
-        Vec::from_slice(env, self.as_slice())
-    }
-}
+pub use testutils::*;
 
 pub trait ThenOk<T, E> {
     fn then_ok(self, ok: T, err: E) -> Result<T, E>;
@@ -21,5 +8,23 @@ pub trait ThenOk<T, E> {
 impl<T, E, F: FnOnce(T) -> Result<T, E>> ThenOk<T, E> for bool {
     fn then_ok(self, ok: T, err: E) -> Result<T, E> {
         self.then_some(ok).ok_or(err)
+    }
+}
+
+#[cfg(any(test, feature = "testutils"))]
+mod testutils {
+    extern crate std;
+
+    use soroban_sdk::{Env, IntoVal, TryFromVal, Val, Vec};
+
+
+    pub trait IntoVec<T> {
+        fn into_vec(self, env: &Env) -> Vec<T>;
+    }
+
+    impl<T: Clone + IntoVal<Env, Val> + TryFromVal<Env, Val>> IntoVec<T> for std::vec::Vec<T> {
+        fn into_vec(self, env: &Env) -> Vec<T> {
+            Vec::from_slice(env, self.as_slice())
+        }
     }
 }
