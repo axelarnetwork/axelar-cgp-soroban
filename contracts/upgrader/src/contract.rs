fce4965c5dae5990e89c77af052819c5c10ac931
Christian Gorenflo <christian@axelar.network>
2024-11-21 12:44:36 -0500

    remove caller param from upgrade contract

--- contracts/upgrader/src/contract.rs
+++ contracts/upgrader/src/contract.rs
@@ -17,37 +17,28 @@ impl Upgrader {
 
     pub fn upgrade(
         env: Env,
-        caller: Address,
         contract_address: Address,
         new_version: String,
         new_wasm_hash: BytesN<32>,
         migration_data: soroban_sdk::Vec<Val>,
     ) -> Result<(), ContractError> {
-        caller.require_auth();
+        ensure_new_version_is_different(&env, &contract_address, &new_version)?;
 
-        // run the upgrade and migration in the context of the given caller,
-        // so to the upgradeable contract it looks like the caller invoked those functions directly,
-        // and can authenticate them correctly
-        env.as_contract(&caller, || {
-            ensure_new_version_is_different(&env, &contract_address, &new_version)?;
+        // it's safe to map the true return value of the upgrade and migrate functions to (),
+        // because we don't care about it, and in case of failure the contract will panic anyway
+        env.invoke_contract::<()>(
+            &contract_address,
+            &Symbol::new(&env, UPGRADE),
+            soroban_sdk::vec![&env, new_wasm_hash.into()],
+        );
 
-            // it's safe to map the true return value of the upgrade and migrate functions to (),
-            // because we don't care about it, and in case of failure the contract will panic anyway
+        env.invoke_contract::<()>(
+            &contract_address,
+            &Symbol::new(&env, MIGRATE),
+            migration_data,
+        );
 
-            env.invoke_contract::<()>(
-                &contract_address,
-                &Symbol::new(&env, UPGRADE),
-                soroban_sdk::vec![&env, new_wasm_hash.into()],
-            );
-
-            env.invoke_contract::<()>(
-                &contract_address,
-                &Symbol::new(&env, MIGRATE),
-                migration_data,
-            );
-
-            ensure_new_version_matches_expected(&env, &contract_address, &new_version)
-        })
+        ensure_new_version_matches_expected(&env, &contract_address, &new_version)
     }
 }
 
@@ -80,7 +71,7 @@ fn current_version(env: &Env, contract_address: &Address) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::contract::{Upgrader, UpgraderClient, UPGRADE, VERSION};
+    use crate::contract::{Upgrader, UpgraderClient, MIGRATE, UPGRADE, VERSION};
     use axelar_soroban_std::UpgradeableInterface;
     use soroban_sdk::testutils::{Address as _, MockAuth, MockAuthInvoke};
     use soroban_sdk::{contract, contracterror, contractimpl, contracttype, Address, Symbol};
@@ -174,24 +165,22 @@ mod tests {
         let expected_data = String::from_str(&env, "migration successful");
         let expected_version = String::from_str(&env, "0.2.0");
 
-        let params = upgrade_call_params(
-            &env,
-            &upgrader_address,
-            &owner,
-            &contract_address,
-            &expected_version,
-            &hash_after_upgrade,
-            &expected_data,
-        );
+        let (upgrade_auth, migrate_auth) =
+            build_invocation_auths(&env, &contract_address, &hash_after_upgrade, &expected_data);
 
         // add the owner to the set of authenticated addresses
-        env.mock_auths(&[MockAuth {
-            address: &owner,
-            invoke: &params,
-        }]);
+        env.mock_auths(&[
+            MockAuth {
+                address: &owner,
+                invoke: &upgrade_auth,
+            },
+            MockAuth {
+                address: &owner,
+                invoke: &migrate_auth,
+            },
+        ]);
 
         UpgraderClient::new(&env, &upgrader_address).upgrade(
-            &owner,
             &contract_address,
             &expected_version,
             &hash_after_upgrade,
@@ -215,110 +204,46 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn upgrade_fails_if_caller_param_does_not_match_actual_caller() {
+    fn upgrade_fails_if_caller_is_authenticated_but_not_owner() {
         let env = Env::default();
 
         let contract_address = env.register(DummyContract, ());
         let upgrader_address = env.register(Upgrader, ());
 
-        let owner = set_owner(&env, &contract_address);
+        let _owner = set_owner(&env, &contract_address);
 
         let hash_after_upgrade = env.deployer().upload_contract_wasm(WASM_AFTER_UPGRADE);
         let expected_data = String::from_str(&env, "migration successful");
         let expected_version = String::from_str(&env, "0.2.0");
 
-        let params = upgrade_call_params(
-            &env,
-            &upgrader_address,
-            &owner,
-            &contract_address,
-            &expected_version,
-            &hash_after_upgrade,
-            &expected_data,
-        );
-
-        // add the owner to the set of authenticated addresses
-        env.mock_auths(&[MockAuth {
-            address: &owner,
-            invoke: &params,
-        }]);
-
-        // should panic: call the upgrade contract as the owner, but pass a wrong address that is not authenticated
-        let wrong_param = Address::generate(&env);
-        env.as_contract(&owner, || {
-            UpgraderClient::new(&env, &upgrader_address).upgrade(
-                &wrong_param,
-                &contract_address,
-                &expected_version,
-                &hash_after_upgrade,
-                &soroban_sdk::vec![&env, expected_data.to_val()],
-            )
-        });
-    }
-
-    #[test]
-    #[should_panic]
-    fn upgrade_fails_if_caller_is_not_owner() {
-        let env = Env::default();
-
-        let contract_address = env.register(DummyContract, ());
-        let upgrader_address = env.register(Upgrader, ());
-
-        let owner = set_owner(&env, &contract_address);
-
-        let hash_after_upgrade = env.deployer().upload_contract_wasm(WASM_AFTER_UPGRADE);
-        let expected_data = String::from_str(&env, "migration successful");
-        let expected_version = String::from_str(&env, "0.2.0");
-
-        let params = upgrade_call_params(
-            &env,
-            &upgrader_address,
-            &owner,
-            &contract_address,
-            &expected_version,
-            &hash_after_upgrade,
-            &expected_data,
-        );
-
-        // add the owner to the set of authenticated addresses
-        env.mock_auths(&[MockAuth {
-            address: &owner,
-            invoke: &params,
-        }]);
-
-        let caller = Address::generate(&env);
-        let params = upgrade_call_params(
-            &env,
-            &upgrader_address,
-            &caller,
-            &contract_address,
-            &expected_version,
-            &hash_after_upgrade,
-            &expected_data,
-        );
+        let (upgrade_auth, migrate_auth) =
+            build_invocation_auths(&env, &contract_address, &hash_after_upgrade, &expected_data);
 
         // add the caller to the set of authenticated addresses
-        env.mock_auths(&[MockAuth {
-            address: &caller,
-            invoke: &params,
-        }]);
+        let caller = Address::generate(&env);
+        env.mock_auths(&[
+            MockAuth {
+                address: &caller,
+                invoke: &upgrade_auth,
+            },
+            MockAuth {
+                address: &caller,
+                invoke: &migrate_auth,
+            },
+        ]);
 
-        // should panic: both caller and owner are authenticated, but the upgradeable contract only accepts the owner,
-        // and the upgrade contract runs the upgrade in the context of the caller
-        env.as_contract(&caller, || {
-            UpgraderClient::new(&env, &upgrader_address).upgrade(
-                &caller,
-                &contract_address,
-                &expected_version,
-                &hash_after_upgrade,
-                &soroban_sdk::vec![&env, expected_data.to_val()],
-            )
-        });
+        // should panic: caller is authenticated, but not the owner
+        UpgraderClient::new(&env, &upgrader_address).upgrade(
+            &contract_address,
+            &expected_version,
+            &hash_after_upgrade,
+            &soroban_sdk::vec![&env, expected_data.to_val()],
+        );
     }
 
     #[test]
     #[should_panic]
-    fn upgrade_fails_if_caller_does_neither_match_caller_param_not_owner() {
+    fn upgrade_fails_if_correct_owner_is_not_authenticated_for_full_invocation_tree() {
         let env = Env::default();
 
         let contract_address = env.register(DummyContract, ());
@@ -330,64 +255,49 @@ mod tests {
         let expected_data = String::from_str(&env, "migration successful");
         let expected_version = String::from_str(&env, "0.2.0");
 
+        let (upgrade_auth, migrate_auth) =
+            build_invocation_auths(&env, &contract_address, &hash_after_upgrade, &expected_data);
+
+        // only add the owner to the set of authenticated addresses for the upgrade function, and the caller for the migrate function
         let caller = Address::generate(&env);
-        let wrong_caller_param = Address::generate(&env);
+        env.mock_auths(&[
+            MockAuth {
+                address: &owner,
+                invoke: &upgrade_auth,
+            },
+            MockAuth {
+                address: &caller,
+                invoke: &migrate_auth,
+            },
+        ]);
 
-        // authenticate all addresses
-        [&owner, &caller, &wrong_caller_param]
-            .iter()
-            .for_each(|address| {
-                let params = upgrade_call_params(
-                    &env,
-                    &upgrader_address,
-                    address,
-                    &contract_address,
-                    &expected_version,
-                    &hash_after_upgrade,
-                    &expected_data,
-                );
-
-                // add the owner to the set of authenticated addresses
-                env.mock_auths(&[MockAuth {
-                    address,
-                    invoke: &params,
-                }])
-            });
-
-        // should panic: while all addresses are authenticated, they must all match for the invocation to succeed
-        env.as_contract(&caller, || {
-            UpgraderClient::new(&env, &upgrader_address).upgrade(
-                &wrong_caller_param,
-                &contract_address,
-                &expected_version,
-                &hash_after_upgrade,
-                &soroban_sdk::vec![&env, expected_data.to_val()],
-            )
-        });
+        UpgraderClient::new(&env, &upgrader_address).upgrade(
+            &contract_address,
+            &expected_version,
+            &hash_after_upgrade,
+            &soroban_sdk::vec![&env, expected_data.to_val()],
+        );
     }
 
-    fn upgrade_call_params<'a>(
+    fn build_invocation_auths<'a>(
         env: &Env,
-        upgrader_address: &'a Address,
-        owner: &'a Address,
         contract_address: &'a Address,
-        expected_version: &'a String,
         hash_after_upgrade: &'a BytesN<32>,
         expected_data: &'a String,
-    ) -> MockAuthInvoke<'a> {
-        MockAuthInvoke {
-            contract: upgrader_address,
+    ) -> (MockAuthInvoke<'a>, MockAuthInvoke<'a>) {
+        let upgrade = MockAuthInvoke {
+            contract: contract_address,
             fn_name: UPGRADE,
-            args: soroban_sdk::vec![
-                &env,
-                owner.to_val(),
-                contract_address.to_val(),
-                expected_version.to_val(),
-                hash_after_upgrade.to_val(),
-                soroban_sdk::vec![&env, expected_data.to_val()].to_val()
-            ],
+            args: soroban_sdk::vec![&env, hash_after_upgrade.to_val()],
             sub_invokes: &[],
-        }
+        };
+        let migrate = MockAuthInvoke {
+            contract: contract_address,
+            fn_name: MIGRATE,
+            args: soroban_sdk::vec![&env, expected_data.to_val()],
+            sub_invokes: &[],
+        };
+        (upgrade, migrate)
     }
 
     fn query_version(env: &Env, contract_address: &Address) -> String {
--- contracts/upgrader/src/lib.rs
+++ contracts/upgrader/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+extern crate alloc;
 
 pub mod contract;
 pub mod error;
