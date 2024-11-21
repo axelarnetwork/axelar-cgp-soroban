5d30652731ed9132cdbdc7d81cd5a94493173f00
Christian Gorenflo <christian@axelar.network>
2024-11-21 12:58:40 -0500

    add doc comments

--- packages/axelar-soroban-std/src/upgrade.rs
+++ packages/axelar-soroban-std/src/upgrade.rs
@@ -2,6 +2,10 @@ use soroban_sdk::{BytesN, Env, String};
 
 pub trait UpgradeableInterface {
     type Error: Into<soroban_sdk::Error>;
+
+    /// Returns the current version of the contract.
     fn version(env: Env) -> String;
+
+    /// Upgrades the contract to a new WASM hash.
     fn upgrade(env: Env, new_wasm_hash: BytesN<32>) -> Result<(), Self::Error>;
 }
