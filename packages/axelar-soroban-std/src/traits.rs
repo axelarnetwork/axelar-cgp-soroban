0b2d38ab5cc8895e6038113db2e1f391e555b4fd
Attiss Ngo <92927591+AttissNgo@users.noreply.github.com>
2024-12-10 07:07:05 -0600

    refactor(its): add BytesExt trait (#108)

--- packages/axelar-soroban-std/src/traits.rs
+++ packages/axelar-soroban-std/src/traits.rs
@@ -14,8 +14,9 @@ impl<T, E> ThenOk<T, E> for bool {
 #[cfg(any(test, feature = "testutils"))]
 mod testutils {
     extern crate std;
+    use std::vec::Vec as StdVec;
 
-    use soroban_sdk::{Env, IntoVal, TryFromVal, Val, Vec};
+    use soroban_sdk::{Bytes, Env, IntoVal, TryFromVal, Val, Vec};
 
     pub trait IntoVec<T> {
         fn into_vec(self, env: &Env) -> Vec<T>;
@@ -24,6 +25,17 @@ mod testutils {
     impl<T: Clone + IntoVal<Env, Val> + TryFromVal<Env, Val>> IntoVec<T> for std::vec::Vec<T> {
         fn into_vec(self, env: &Env) -> Vec<T> {
             Vec::from_slice(env, self.as_slice())
+        }
+    }
+
+    pub trait BytesExt {
+        fn from_hex(env: &Env, hex_string: &str) -> Bytes;
+    }
+
+    impl BytesExt for Bytes {
+        fn from_hex(env: &Env, hex_string: &str) -> Bytes {
+            let bytes_vec: StdVec<u8> = hex::decode(hex_string).expect("hex decoding failed");
+            Self::from_slice(env, &bytes_vec)
         }
     }
 }
