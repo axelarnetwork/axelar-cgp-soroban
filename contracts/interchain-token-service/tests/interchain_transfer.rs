b3fd5c0caf3c68d457a7708f60510a85b4ad6866
Attiss Ngo <92927591+AttissNgo@users.noreply.github.com>
2024-12-13 12:54:44 -0600

    feat(its): update event test with new event test utils
    - fix clippy warnings in auth.rs test file

--- contracts/interchain-token-service/tests/interchain_transfer.rs
+++ contracts/interchain-token-service/tests/interchain_transfer.rs
@@ -82,11 +82,11 @@ fn interchain_transfer_receive() {
     let msg = HubMessage::ReceiveFromHub {
         source_chain: String::from_str(&env, HUB_CHAIN),
         message: Message::InterchainTransfer(InterchainTransfer {
-            token_id: token_id,
+            token_id,
             source_address: sender,
             destination_address: recipient,
             amount,
-            data: data,
+            data,
         }),
     };
     let payload = msg.abi_encode(&env).unwrap();
