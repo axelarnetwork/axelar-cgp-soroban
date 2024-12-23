8eba3199c09180f7db446ddcc25580ad935fbfcc
Attiss Ngo <92927591+AttissNgo@users.noreply.github.com>
2024-12-23 14:35:37 -0600

    chore: simplify auth test utils (#125)

--- contracts/interchain-token-service/tests/deploy_remote_interchain_token.rs
+++ contracts/interchain-token-service/tests/deploy_remote_interchain_token.rs
@@ -1,63 +1,18 @@
 mod utils;
 
-use axelar_soroban_std::assert_contract_err;
-use axelar_soroban_std::auth_invocation;
-use axelar_soroban_std::events;
-use interchain_token_service::error::ContractError;
-use interchain_token_service::event::InterchainTokenDeploymentStartedEvent;
-use interchain_token_service::types::DeployInterchainToken;
-use interchain_token_service::types::HubMessage;
-use interchain_token_service::types::Message;
-use soroban_sdk::testutils::AuthorizedFunction;
-use soroban_sdk::testutils::AuthorizedInvocation;
-use soroban_sdk::Bytes;
-use soroban_sdk::IntoVal;
-use soroban_sdk::Symbol;
-use soroban_sdk::{testutils::Address as _, Address, BytesN, String};
+use axelar_soroban_std::{assert_contract_err, auth_invocation, events};
+use interchain_token_service::{
+    error::ContractError,
+    event::InterchainTokenDeploymentStartedEvent,
+    types::{DeployInterchainToken, HubMessage, Message},
+};
+use soroban_sdk::testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation};
+use soroban_sdk::{Address, Bytes, BytesN, IntoVal, String, Symbol};
 use soroban_token_sdk::metadata::TokenMetadata;
-use utils::setup_env;
-use utils::setup_gas_token;
-use utils::TokenMetadataExt;
+use utils::{setup_env, setup_gas_token, TokenMetadataExt};
 
 #[test]
 fn deploy_remote_interchain_token_succeeds() {
-    let (env, client, _, _, _) = setup_env();
-
-    let sender = Address::generate(&env);
-    let gas_token = setup_gas_token(&env, &sender);
-    let minter: Option<Address> = None;
-    let salt = BytesN::<32>::from_array(&env, &[1; 32]);
-    let token_metadata = TokenMetadata::new(&env, "name", "symbol", 6);
-    let initial_supply = 1;
-
-    let token_id = client.mock_all_auths().deploy_interchain_token(
-        &sender,
-        &salt,
-        &token_metadata,
-        &initial_supply,
-        &minter,
-    );
-
-    let destination_chain = String::from_str(&env, "ethereum");
-    client
-        .mock_all_auths()
-        .set_trusted_chain(&destination_chain);
-
-    let deployed_token_id = client.mock_all_auths().deploy_remote_interchain_token(
-        &sender,
-        &salt,
-        &destination_chain,
-        &gas_token,
-    );
-    assert_eq!(token_id, deployed_token_id);
-
-    goldie::assert!(events::fmt_emitted_event_at_idx::<
-        InterchainTokenDeploymentStartedEvent,
-    >(&env, -4));
-}
-
-#[test]
-fn deploy_remote_interchain_token_auth_test() {
     let (env, client, _, gas_service, _) = setup_env();
 
     let sender = Address::generate(&env);
@@ -89,7 +44,12 @@ fn deploy_remote_interchain_token_auth_test() {
         &destination_chain,
         &gas_token,
     );
+
     assert_eq!(token_id, deployed_token_id);
+
+    goldie::assert!(events::fmt_emitted_event_at_idx::<
+        InterchainTokenDeploymentStartedEvent,
+    >(&env, -4));
 
     let message = Message::DeployInterchainToken(DeployInterchainToken {
         token_id,
@@ -98,27 +58,22 @@ fn deploy_remote_interchain_token_auth_test() {
         decimals: token_metadata.decimal as u8,
         minter: None,
     });
-
     let payload = HubMessage::SendToHub {
         destination_chain: destination_chain.clone(),
         message,
     }
     .abi_encode(&env);
 
-    let transfer_auth = auth_invocation!(&env,
-        "transfer",
-        gas_token.clone().address =>
-        (
-            &sender,
-            gas_service.address.clone(),
-            gas_token.amount
-        )
+    let transfer_auth = auth_invocation!(
+        &env,
+        sender,
+        gas_token.transfer(&sender, gas_service.address.clone(), gas_token.amount)
     );
 
-    let pay_gas_auth = auth_invocation!(&env,
-        "pay_gas",
-        gas_service.address =>
-        (
+    let pay_gas_auth = auth_invocation!(
+        &env,
+        sender,
+        gas_service.pay_gas(
             client.address.clone(),
             its_hub_chain,
             its_hub_address,
@@ -130,16 +85,10 @@ fn deploy_remote_interchain_token_auth_test() {
         transfer_auth
     );
 
-    let deploy_remote_interchain_token_auth = auth_invocation!(&env,
+    let deploy_remote_interchain_token_auth = auth_invocation!(
+        &env,
         sender,
-        "deploy_remote_interchain_token",
-        client.address =>
-        (
-            &sender,
-            salt,
-            destination_chain,
-            gas_token
-        ),
+        client.deploy_remote_interchain_token(&sender, salt, destination_chain, gas_token),
         pay_gas_auth
     );
 
--- packages/axelar-soroban-std/src/testutils.rs
+++ packages/axelar-soroban-std/src/testutils.rs
@@ -71,43 +71,71 @@ where
     assert_emitted_event(env, -1, contract_id, topics, data);
 }
 
+/// Helper macro for building and verifying authorization chains in Soroban contract tests.
+///
+/// Used to verify that contract calls require the correct sequence of authorizations.
+/// See the example package for usage in gas payment and cross-chain message verification scenarios.
+///
+/// # Example
+/// ```rust,ignore
+/// // Create authorization for a token transfer
+/// let transfer_auth = auth_invocation!(
+///     &env,
+///     user,
+///     asset_client.transfer(
+///         &user,
+///         source_gas_service_id,
+///         gas_token.amount
+///     )
+/// );
+///
+/// // Create nested authorization chain for gas payment
+/// let pay_gas_auth = auth_invocation!(
+///     &env,
+///     user,
+///     source_gas_service_client.pay_gas(
+///         source_app.address,
+///         destination_chain,
+///         destination_address,
+///         payload,
+///         &user,
+///         gas_token,
+///         &Bytes::new(&env)
+///     ),
+///     transfer_auth
+/// );
+///
+/// // Verify authorizations
+/// assert_eq!(env.auths(), pay_gas_auth);
+/// ```
 #[macro_export]
 macro_rules! auth_invocation {
-    // Normal invocation: auth!(env, "function_name", address => args)
-    ($env:expr, $fn:expr, $addr:expr => $args:expr) => {{
-        std::vec![AuthorizedInvocation {
-            function: AuthorizedFunction::Contract((
-                $addr,
-                Symbol::new($env, $fn),
-                $args.into_val($env),
-            )),
-            sub_invocations: std::vec![],
-        }]
-    }};
-
-    // Invocation with sub-invocations: auth!(env, "function_name", address => args, subs)
-    ($env:expr, $fn:expr, $addr:expr => $args:expr, $subs:expr) => {{
-        std::vec![AuthorizedInvocation {
-            function: AuthorizedFunction::Contract((
-                $addr,
-                Symbol::new($env, $fn),
-                $args.into_val($env),
-            )),
-            sub_invocations: $subs,
-        }]
-    }};
-
-    // User invocation: auth!(env, user, "function_name", address => args, subs)
-    ($env:expr, $user:expr, $fn:expr, $addr:expr => $args:expr, $subs:expr) => {{
+    // Basic case without sub-invocations
+    ($env:expr, $caller:expr, $client:ident.$method:ident($($arg:expr),* $(,)?)) => {{
         std::vec![(
-            $user.clone(),
+            $caller.clone(),
             AuthorizedInvocation {
                 function: AuthorizedFunction::Contract((
-                    $addr,
-                    Symbol::new($env, $fn),
-                    $args.into_val($env),
+                    $client.address.clone(),
+                    Symbol::new($env, stringify!($method)),
+                    ($($arg),*).into_val($env),
                 )),
-                sub_invocations: $subs
+                sub_invocations: std::vec![],
+            }
+        )]
+    }};
+
+    // Case with sub-invocations (handles both regular and user auth cases)
+    ($env:expr, $caller:expr, $client:ident.$method:ident($($arg:expr),* $(,)?), $subs:expr $(, $user:ident)?) => {{
+        std::vec![(
+            $caller.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    $client.address.clone(),
+                    Symbol::new($env, stringify!($method)),
+                    ($($arg),*).into_val($env),
+                )),
+                sub_invocations: $subs.into_iter().map(|(_, inv)| inv).collect(),
             }
         )]
     }};
